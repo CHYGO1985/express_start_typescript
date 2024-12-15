14b27a869f454f37214c82b657a3d00b8d5e2efd
CHYGO1985 <jingjiejiang85@gmail.com>
2024-12-15 13:58:46 +0100

    Runnable express typescript start project

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import createError from 'http-errors'
+import express, { Request, Response, NextFunction } from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import { fileURLToPath } from 'url'
+
+import indexRouter from './routes/index.js'
+import usersRouter from './routes/users.js'
+
+const app = express()
+
+const filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(filename)
+
+// view engine setup
+app.set('views', path.join(__dirname, '../src', 'views'))
+app.set('view engine', 'jade')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+export default app
